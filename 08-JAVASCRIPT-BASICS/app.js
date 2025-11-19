64093b88cc928da2b5a78174c4faaa488723a6a3
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-11-19 16:27:21 +0000

    completed code

---
+++ 08-JAVASCRIPT-BASICS/app.js
@@ -0,0 +1,23 @@
+const myFirstName = "John";
+const mySurname = "Doe";
+let myAge = 21;
+const myYearOfBirth = 2005;
+let myCityOfResidence = "London";
+
+console.log(myFirstName);
+console.log(mySurname);
+console.log(myAge);
+console.log(myYearOfBirth);
+console.log(myCityOfResidence);
+
+let numOne = 5;
+let numTwo = 10;
+
+let sum = numOne + numTwo;
+
+console.log("Sum: " + sum);
+
+console.log(`My name is ${myFirstName} ${mySurname}, I am ${myAge} years old, I was born in ${myYearOfBirth} and I live in ${myCityOfResidence}.`);
+
+numOne = 10;
+numTwo = 5;
