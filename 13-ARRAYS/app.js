a32ffbef17b3c28f024cacb58f8ceca45b4406df
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-11-24 10:43:10 +0000

    build: completed code

---
+++ 13-ARRAYS/app.js
@@ -0,0 +1,30 @@
+console.log("Hello world!");
+const blogPosts = [
+    "My first blog post",
+    "My second blog post",
+    "My third blog post",
+];
+
+console.log(blogPosts);
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
+
+const randomList = ["important reminder", 42, ["another array"], true];
+console.log(randomList);
+
+const favouriteFoods = ["Lasagne", "Sushi", "Pizza"];
+console.log(favouriteFoods);
+
+favouriteColours = ["Blue", "Green", "Red"];
+console.log(favouriteColours);
+
+favouriteNumbers = [7, 100, 3.14];
+console.log(favouriteNumbers);
+
+console.log(favouriteFoods[0], favouriteColours[1], favouriteNumbers[2]);;
