55a7d2c411b91b443d7c535a9b082df614aee4c0
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-11-24 14:19:19 +0000

    build: code complete

--- 10-FUNCTIONS/app.js
+++ 10-FUNCTIONS/app.js
@@ -1,3 +1,36 @@
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    return a / b;
+}
+
+function calculate(a,b,operator){
+    switch (operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+        default:
+            return "Invalid operator";
+    }
+}
+
+let a = 5;
+let b = 10;
+let operator = "+";
+console.log(calculate(a,b,operator));
+
 // miles accepts a number.
 // convertTo accepts a string (either "kilometers" or "miles").
 function convertDistance(miles, convertTo){
