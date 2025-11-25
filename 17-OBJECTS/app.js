d9fb0c8cb120d23cc7e91c8f4351582e494328d0
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-11-25 11:00:34 +0000

    build: completed code

---
+++ 17-OBJECTS/app.js
@@ -0,0 +1,17 @@
+const car = {
+    make: 'Toyota',
+    model: 'Corolla',
+    colour: 'blue',
+};
+
+let myString = `I drive a ${car.colour} ${car.make} ${car.model}.`;
+
+console.log(myString);
+
+const book = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    pages: 180,
+};
+
+console.log(`"${book.title}" by ${book.author} has ${book.pages} pages.`);
---
+++ 17-OBJECTS/demo.js
@@ -0,0 +1,13 @@
+// structure
+const objectName = {
+    key1: `value`,
+    key2: `value`,
+    key3: `value`,
+}
+
+//example
+const city = {
+    name: `New York`,
+    country: `USA`,
+    population: `8 million`,
+}
