7cd3d270d05e43c575344637cf9d500b0d7e9365
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-01 11:50:23 +0000

    build: code complete

--- 24-FORMS/app.js
+++ 24-FORMS/app.js
@@ -2,7 +2,16 @@ const userForm = document.getElementById("userForm");
 userForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(userForm);
-    console.log(formData);
+    // console.log(formData);
     const userEntries = Object.fromEntries(formData);
-    console.log(userEntries);
+    const userDisplayName = document.createElement("h2");
+    const userDisplayCity = document.createElement("h2");
+    const userDisplayHobby = document.createElement("h2");
+    userDisplayName.innerText = userEntries.userName;
+    userDisplayCity.innerText = userEntries.city;
+    userDisplayHobby.innerText = userEntries.hobby;
+    document.body.appendChild(userDisplayName);
+    document.body.appendChild(userDisplayCity);
+    document.body.appendChild(userDisplayHobby);
 });
+
