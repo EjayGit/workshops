920e2e58e77d962c7c8fa2435cb814cbe9b5cd6e
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-01 17:38:04 +0000

    build: mini challenge completed minus stretch.

--- 25-FETCH/app.js
+++ 25-FETCH/app.js
@@ -1,21 +1,20 @@
 console.log("Hello universe");
 
+async function getMyStuffFromOverThere(){
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const json = await response.json();
+  console.log(json);
+}
 
-// async function getMyStuffFromOverThere(){
-//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-//   const json = await response.json();
-//   console.log(json);
-// }
+getMyStuffFromOverThere();
 
-// getMyStuffFromOverThere();
+async function getViews(){
+    const response = await fetch("https://api.github.com/repos/EjayGit/Object-Tracking");
+    const json = await response.json();
+    console.log(json.stargazers_count);
+    const domElement = document.createElement("h2");
+    domElement.innerText = `The number of current views is: ${json.stargazers_count}`;
+    document.body.appendChild(domElement);
+}
 
-// async function getViews(){
-//     const response = await fetch("https://api.github.com/repos/EjayGit/Object-Tracking");
-//     const json = await response.json();
-//     console.log(json.stargazers_count);
-//     const domElement = document.createElement("h2");
-//     domElement.innerText = json.stargazers_count;
-//     document.body.appendChild(domElement);
-// }
-
-// getViews();
+getViews();
