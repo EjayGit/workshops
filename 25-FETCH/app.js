5930d38c47930ec37c7fba929cc99050a20a7615
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-01 17:16:26 +0000

    build: update code and test commit

--- 25-FETCH/app.js
+++ 25-FETCH/app.js
@@ -1,8 +1,19 @@
-async function getMyStuffFromOverThere() {
+console.log("Hello universe")
+
+async function getMyStuffFromOverThere(){
   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  console.log("HTTP Response:", response);
   const json = await response.json();
-  console.log("JSON Data:", json);
 }
 
 getMyStuffFromOverThere();
+
+async function getViews(){
+    const response = await fetch("https://api.github.com/repos/EjayGit/Object-Tracking");
+    const json = await response.json();
+    console.log(json.stargazers_count);
+    const domElement = document.createElement("h2");
+    domeElement.innerText = json.stargazers_count;
+    document.body.appendChild(domElement);
+}
+
+getViews()
