27915d28df84fef1ef897ab0ea3fd261ff822934
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-01 17:30:40 +0000

    build: update code and test commit

--- 25-FETCH/app.js
+++ 25-FETCH/app.js
@@ -1,19 +1,21 @@
-console.log("Hello universe")
+console.log("Hello universe");
 
-async function getMyStuffFromOverThere(){
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const json = await response.json();
-}
 
-getMyStuffFromOverThere();
+// async function getMyStuffFromOverThere(){
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   const json = await response.json();
+//   console.log(json);
+// }
 
-async function getViews(){
-    const response = await fetch("https://api.github.com/repos/EjayGit/Object-Tracking");
-    const json = await response.json();
-    console.log(json.stargazers_count);
-    const domElement = document.createElement("h2");
-    domeElement.innerText = json.stargazers_count;
-    document.body.appendChild(domElement);
-}
+// getMyStuffFromOverThere();
 
-getViews()
+// async function getViews(){
+//     const response = await fetch("https://api.github.com/repos/EjayGit/Object-Tracking");
+//     const json = await response.json();
+//     console.log(json.stargazers_count);
+//     const domElement = document.createElement("h2");
+//     domElement.innerText = json.stargazers_count;
+//     document.body.appendChild(domElement);
+// }
+
+// getViews();
