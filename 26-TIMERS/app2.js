4b419b79a52ec69cf59dc36b258c51fdb3d8b697
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2025-12-02 10:37:15 +0000

    build: start of mini challenge 2

---
+++ 26-TIMERS/app2.js
@@ -0,0 +1,11 @@
+const clickButton = document.getElementById('btn');
+const stopwatch = document.getElementById('stopwatch')
+let buttonClicked = 0;
+
+clickButton.addEventListener("click", buttonHandler(btnClicked));
+
+function btnClicked(buttonClicked) {
+    if (buttonClicked === 0){
+
+    }
+}
