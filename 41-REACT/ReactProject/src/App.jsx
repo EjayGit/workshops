c838ee34d5b4c51ce8304d35ab99a688fe884641
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-01-05 14:30:37 +0000

    build:: React set up and components example.

---
+++ 41-REACT/ReactProject/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const name = 'Me';
+
+function App() {
+  	return (
+    	<>
+      		<div>
+        	<h1>Hello, world!</h1>
+			<p>The sum of 1 + 2 = {1+2}.</p>
+			<p>My name is {name}.</p>
+      		</div>
+    	</>
+	);
+}
+
+export default App;
