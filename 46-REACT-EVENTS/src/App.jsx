53c9bae0887e7e35f94118b56d341ff1024732ec
ERIK J CLARK <ERIKJCLARK@ICLOUD.COM>
2026-01-06 13:34:48 +0000

    build: react events

--- 46-REACT-EVENTS/src/App.jsx
+++ 46-REACT-EVENTS/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Version 1
+// function App() {
+  
+//     function handleClick(){
+//       console.log("I was clicked!");
+//     }
 
-function App() {
-  const [count, setCount] = useState(0)
+//   return (
+//     <>
+    
+//       <h1 onClick={handleClick}>Event Handler</h1>
+//       <button onClick={handleClick}>Click me!</button>
+
+//     </>
+//   )
+// }
+
+// Version 2
+// function App(){
+//   return (
+//     <>
+//       <h1>Inline events</h1>
+//       <button onClick={() => console.log("I was clicked!")}>Arrow!</button>
+
+//       <button
+//         onClick={function() {
+//           console.log("I was clicked!")
+//         }}
+//         >Normal!</button>
+//     </>
+//   )
+// }
+
+// Version 3
+function App(){
+  
+  function handleClick(username){
+    console.log(`Hello ${username}`);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Greeting</h1>
+      <button onClick={() => handleClick("Bertie")}>Greet Bertie!</button>
+
+      <button
+        onClick={function() {
+          console.log("I was clicked!")
+        }}
+        >Normal!</button>
     </>
   )
 }
